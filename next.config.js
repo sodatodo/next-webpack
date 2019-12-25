822db045d6a58df5d773451121179c6d398acc4e
soda <stacksoda@163.com>
2019-12-25 14:21:30 +0800

    fix: import css

--- next.config.js
+++ next.config.js
@@ -2,23 +2,23 @@
  * @Author: soda
  * @Date: 2019-12-25 09:36:36
  * @LastEditors  : soda
- * @LastEditTime : 2019-12-25 11:54:30
+ * @LastEditTime : 2019-12-25 14:11:25
  */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (phase, {defaultConfig}) => {
-    // fix: prevents error when .less files are required by node
-    if (typeof require !== 'undefined') {
-      require.extensions['.less'] = file => {}
-    }
+module.exports = (phase, { defaultConfig }) => {
+  // fix: prevents error when .less files are required by node 重要
+  if (typeof require !== 'undefined') { // 在浏览器端 require 为 undefined
+    require.extensions['.less'] = file => { }
+  }
   return {
     webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-      
+
       config.plugins.push(new MiniCssExtractPlugin({
         filename: '[name].[contenthash].css',
         chunkFilename: '[id].[contenthash].css',
       }));
-      
+
       config.module.rules.push({
         test: /\.css$/,
         use: [
@@ -26,7 +26,7 @@ module.exports = (phase, {defaultConfig}) => {
           'css-loader'
         ]
       })
-  
+
       config.module.rules.push({
         test: /\.less$/,
         use: [
@@ -41,7 +41,34 @@ module.exports = (phase, {defaultConfig}) => {
           }
         ]
       })
-      
+      config.module.rules.push({
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader"
+          },
+          "sass-loader"
+        ]
+      })
+
+      // config.module.rules.push({
+      //   test: /\.(png|svg|jpg|gif)$/,
+      //   use: {
+      //     loader: 'url-loader',
+      //     options: {
+      //       limit: 10240,
+      //     }
+      //   }
+      // })
+
+      // config.module.rules.push({
+      //   test: /\.(woff|woff2|eof|ttf|otf)$/,
+      //   use: [
+      //     'file-loader'
+      //   ]
+      // })
+
       return config;
     }
   }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,19 @@
  * @Author: soda
  * @Date: 2019-12-25 09:33:52
  * @LastEditors  : soda
- * @LastEditTime : 2019-12-25 10:51:31
+ * @LastEditTime : 2019-12-25 14:09:34
  */
 import './style.css';
 import './style.less';
+import './style.scss';
 import { Button } from 'antd';
+// import image from './big-images.jpg'
 
-export default () => <div className="bg lessstyle">Welcome to<Button type="primary">Button</Button> next.js!</div>
+export default () => (
+  <div className="bg lessstyle"> Welcome
+  {/* <img src={image} alt="image" />  */}
+  to
+  <Button type="primary">Button</Button> next.js!
+  <div className="test-sass">sass content</div>
+  </div>
+)
